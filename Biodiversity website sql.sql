cfb117b15000c0334c3533eff2f302f9375faba9
Elanur Akbas <elaa.2399@gmail.com>
2022-11-17 19:01:23 +0100

    if table does not exists it creates one

--- Biodiversity website sql.sql
+++ Biodiversity website sql.sql
@@ -35,7 +35,15 @@ END
 
 ELSE
 BEGIN
-create table customer
+create table customer(
+id int primary key identity(1,1), --join to account.customer_id
+first_name varchar(50),
+last_name varchar(50),
+[password] varchar(400),
+age int, 
+email varchar(50) unique,
+gender varchar(20)  --F for female, M for male (O for other)
+)
 END
 
 IF EXISTS(SELECT 1 FROM sys.Objects
@@ -47,7 +55,16 @@ END
 
 ELSE
 BEGIN
-create table account
+create table account(
+id int primary key identity(1,1), --join to files.account_id and feedback.account_id
+employee_id int,--join to employee.id
+customer_id int, --join to customer.id
+islogin bit, --we will turn this off for pushing features (CI/CD concept), prediction? chose bit because it is yes or no
+lastupdated datetime, --when password changed
+lastupdatedreason varchar, --change password/username
+lastupdatedby int, --employee id
+prediction varchar(50) --predict uploaded image, will show most recent prediction
+)
 END
 
 IF EXISTS(SELECT 1 FROM sys.Objects
@@ -59,7 +76,13 @@ END
 
 ELSE
 BEGIN
-create table files
+create table files(
+id int primary key identity(1,1), 
+file_path varchar(max), --path to find file
+file_type varchar(50), --sound, image, image_pp
+date_time datetime, --when is file created
+account_id int --join to account.id
+)
 END
 
 IF EXISTS(SELECT 1 FROM sys.Objects
@@ -71,7 +94,14 @@ END
 
 ELSE
 BEGIN
-create table feedback
+create table feedback(
+id int primary key identity(1,1), 
+account_id int, --join to account.id
+feedback_button varchar(20), 
+feedback_text varchar(200), 
+issue varchar(50), --'in progress' 'solved' or 'cancelled' (could not find anything to just store three options? so varchar is the best option I think)
+solved_by int--join to employee.id
+)
 END
 
 IF EXISTS(SELECT 1 FROM sys.Objects
@@ -83,7 +113,15 @@ END
 
 ELSE
 BEGIN
-create table employee
+create table employee(
+id int primary key identity(1,1), 
+first_name varchar(50),
+last_name varchar(50),
+age int, 
+email varchar(60) unique,
+gender char(1),
+position varchar(30)
+)
 END
 
 IF EXISTS(SELECT 1 FROM sys.Objects
@@ -95,7 +133,12 @@ END
 
 ELSE
 BEGIN
-create table issue
+create table issue(
+id int primary key identity(1,1), 
+issue_ varchar(200),
+[type] varchar(20), --banned or deleted?
+solution_by int --join to employee.id
+)
 END
 
 drop table customer
